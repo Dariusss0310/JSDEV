7e0b0954fb94fac9ae1a6f2861a49ffda3ee86af
Frontend JavaScript <frontendjavascript@C3-12.local>
2023-06-08 20:46:35 +0300

    vueCRUD

---
+++ angularCRUD/frontend/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfilComponent } from './profil/profil.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {ApiService} from './api.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProfilComponent,
+  ],
+  imports:[
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [
+    ApiService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
